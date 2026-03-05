f2c229cc170157f0d4b7d897f72e565047dd46f7
Asrar SB <asrarsb786@gmail.com>
2026-03-05 23:42:06 +0530

    Added logging in transform step

--- src/transform.py
+++ src/transform.py
@@ -1,5 +1,6 @@
 def transform(df):
     df = df.dropna()
     df.columns = df.columns.str.lower()
+    print("Rows after cleaning:", len(df))
     print("Data Transformed Successfully")
     return df
